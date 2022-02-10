a553147f7684cb5668551fe178c51a3f57c66345
kreenatotala <74081275+kreenatotala@users.noreply.github.com>
2022-02-10 03:08:59 -0500

    finished assignment

--- main.js
+++ main.js
@@ -11,4 +11,17 @@ Modified: 12/06/21
     // You will need to modiffy the body of this function as described in the assignment Readme
 function button1Clicked() {
     console.log("Button 1 was clicked"); 
+    document.getElementById("random_num").innerHTML = Math.floor(Math.random() * 1000);
+}
+
+function button2Clicked() {
+    console.log("Button 2 was clicked");
+    if (document.getElementById("circle").style.fill === "blue") {
+        document.getElementById("circle").style.fill = "green";
+    } else if (document.getElementById("circle").style.fill === "green") {
+        document.getElementById("circle").style.fill = "blue";
+    }
+    
+    
+    
 }
